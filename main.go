9238431d01f242eebeaa4bd118cdf88bfd032f2f
ianmm17 <ianmm17@yahoo.com>
2021-10-27 11:23:41 -0500

    WIP start of buildoing interact cli, IM

--- main.go
+++ main.go
@@ -2,41 +2,66 @@ package main
 
 import (
 	"fmt"
-	//"practiceApp/practice"
 	"practiceApp/goProblems"
 )
 
 func main() {
-	choice := "add"
-	doubtInt := goProblems.DoubleInt(2)
-	doubleCollectionResults := goProblems.CollectionSliceInt([]int{5,6,7,})
-	doWeMatch := goProblems.DoWeMatch([]int{1,2,3}, []int{1,2,3})
-	lowHighResults := goProblems.LowestAndHighest([]int{3,4,5}, choice)
-	numberOfLegs := goProblems.NumberOfLegs(2, 2, 1)
-	sumInt := goProblems.SumInt([]int{1,2,3,4})
-	sumIntMulti := goProblems.ProductMulti([]int{1,2,3,4})
-	userChoice := goProblems.UserChoiceAddOrMultiply([]int{1,2,3,4}, false)
-	convertToSeconds := goProblems.ConvertToSeconds(1, 2)
-	minToSec := goProblems.MinToSec(4)
-	hrToSec := goProblems.HrToSec(1)
+	userName := getUserName()
 
+	doYouWantToRun(userName)
+
+}
+
+func listOfGoProblems() {
+	var numChoice string
+	println("1. doubles integer")
+	println("2. doubles each integer in given slice")
+
+	fmt.Scanf("%s", &numChoice)
+	fmt.Println("You have chosen: ", numChoice)
+
+	switch numChoice {
+	case "1":
+		case1()
+	case "2":
+	default:
+		fmt.Println(numChoice, "isn't an option. Please choose from the following.")
+		listOfGoProblems()
+	}
+
+}
+
+func doYouWantToRun(name string) string {
+	var yesOrNo string
+	fmt.Println(name, ", Would you like to run Go problems? y/n")
+	fmt.Scanf("%s", &yesOrNo)
+
+	if yesOrNo == "y" {
+		listOfGoProblems()
+	} else if yesOrNo == "n" {
+		fmt.Println("no")
+	} else {
+		println("Invalid argument please try again!")
+		doYouWantToRun(name)
+	}
+	return yesOrNo
+}
+
+func getUserName() string {
 	var name string
 	fmt.Println("Enter your name: ")
 	fmt.Scanf("%s", &name)
 	fmt.Println("Hello", name)
-	fmt.Println(name, ", Would you like to run Go problems? y/n")
+	return name
+}
 
-	fmt.Println(doubtInt)
-	fmt.Println(doubleCollectionResults)
-	fmt.Println(doWeMatch)
-	fmt.Printf("%v is the lowest and %v is the highest", lowHighResults[0], lowHighResults[1])
-	fmt.Printf("\nYou have chosen %v and the total is: %v", choice, lowHighResults[2])
-	fmt.Printf("\nThe total number of legs on your farm is: %v\n", numberOfLegs)
-	fmt.Println(sumInt)
-	fmt.Println(sumIntMulti)
-	fmt.Println(userChoice)
-	fmt.Println(convertToSeconds)
-	fmt.Println(minToSec)
-	fmt.Println(hrToSec)
-
+func case1() {
+	var passedInt int
+	fmt.Println("Enter Number: ")
+	if _, err := fmt.Scan(&passedInt); err != nil {
+		println("Integer not given, please enter integer")
+		case1()
+	} else {
+		fmt.Println("Your doubled integer is: ", goProblems.DoubleInt(passedInt))
+	}
 }
